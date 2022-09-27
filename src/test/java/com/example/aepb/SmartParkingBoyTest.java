a46d7db3a779cdbfa111ed29e25decee9fa7d6f1
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2022-09-27 14:02:11 +0800

    [zhiheng] test: should success get the empty count max parking lot  a ticket when park car given parking empty count  a6  b3  c5.

--- src/test/java/com/example/aepb/SmartParkingBoyTest.java
+++ src/test/java/com/example/aepb/SmartParkingBoyTest.java
@@ -36,5 +36,19 @@ class SmartParkingBoyTest {
         assertEquals("C", ticket.getParkingLotName());
     }
 
+    @Test
+    void should_success_get_the_empty_count_max_parking_lot_A_ticket_when_park_car_given_parking_empty_count_A_6_B_3_C_5() {
+        ParkingLot parkingLotA = new ParkingLot(6, "A");
+        ParkingLot parkingLotB = new ParkingLot(3, "B");
+        ParkingLot parkingLotC = new ParkingLot(5, "C");
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(List.of(parkingLotA, parkingLotB, parkingLotC));
+        Car car = new Car("鄂A12345");
+
+        Ticket ticket = smartParkingBoy.park(car);
+
+        assertNotNull(ticket);
+        assertEquals("A", ticket.getParkingLotName());
+    }
+
 
 }
